c8d51e2686d68d0ad33225df14d6586eea6e9910
cecil <migayicecil@gmail.com>
2022-09-06 19:20:53 +0300

    Added back up A4 portrait templates

--- app/enterprise/InvoiceTemplate_landscape.php
+++ app/enterprise/InvoiceTemplate_landscape.php
@@ -49,41 +49,41 @@ class InvoiceTemplate{
         if(!empty($KRAQRCodeLink)){
 
             $htmlTemplate = "
-            <div style='width:210mm;height:297mm;margin-left:auto; margin-right:auto;'> 
-                        <table style='width: 100%;'>
+                <div style='padding: 10px;font-size: 14px;'>  
+                        <table style='width: 950px;'>
                             <tr style='text-align: left;'>
-                                <td style='width:50%;'>
-                                    <span style='font-size: 25px; color: #232323; font-weight: bold; font-family: Arial, Helvetica, sans-serif;'>
+                                <td style='width:500px;'>
+                                    <span style='margin-left: 45px; font-size: 25px; color: #232323; font-weight: bold; font-family: Arial, Helvetica, sans-serif;'>
                                         INVOICE + DELIVERY NOTE
                                     </span>
                                 </td>            
-                                <td style='width: 40%;'>
+                                <td style='width: 400px;'>
                                     <img style='margin-left: 150px;' width='150px' height='100px' src='https://images.squarespace-cdn.com/content/v1/5b69f9f37e3c3af551b48958/1559157173218-VJDY1ISN5QYWIOSLS5X7/Logo+-+Grey.png?format=300w'/>
                                 </td>
                             </tr>
                         </table> 
-                    <div style='margin-top: 10px;width: 100%; border:0px solid #000;'>
-                        <table style='width: 100%;'>
+                    <div style='margin-top: 5px; width: 950px; margin-left: 30px'>
+                        <table style='width: 950px;'>
                             <tr style='text-align: left;'>
-                                <th style='width: 35%;text-align: left;'>$customerName</th>
-                                <th style='width: 20%;text-align: left;'>Invoice Date:</th>
-                                <th style='width: 40%;text-align: left;'>Spring Valley Coffee Roasters Limited</th>
+                                <th style='width: 350px;text-align: left;'>$customerName</th>
+                                <th style='width: 200px;text-align: left;'>Invoice Date:</th>
+                                <th style='width: 400px;text-align: left;'>Spring Valley Coffee Roasters Limited</th>
                             </tr>
                             <tr style='text-align: left;'>
-                                <td style='width: 35%;'>
+                                <td style='width: 350px;'>
                                     $postalAddressStreetAddress <br/>
                                     $postalAddressStreetAddress2<br/>  
                                     $postalAddressCity<br/> 
                                     $postalAddressCountry<br/>
                                     $customerGSTVATNumber              
                                 </td>
-                                <td style='width: 20%;'>
+                                <td style='width: 200px;'>
                                     $invoiceDate<br/> 
                                     <b>Invoice #</b><br/>
                                     $invoiceNumber <br/> 
                                     <b>Customer Ref</b>             
                                 </td>
-                                <td style='width: 40%;'>
+                                <td style='width: 400px;'>
                                     Spring Valley Shopping Centre, Shop 5<br/>  
                                     Lower Kabete Road  <br/>
                                     Nairobi Kenya +254 775 111 111 <br/>
@@ -92,14 +92,14 @@ class InvoiceTemplate{
                                 </td>
                             </tr>
                         </table>
-                        <table style='width: 100%;margin-top: 20px;border-collapse: collapse;'>
+                        <table style='width: 950px;margin-top: 20px; border-collapse: collapse;'>
                             <tr style='text-align: left; border-bottom: 2px solid rgb(122, 120, 120);'>
-                                <th style='width: 35%;padding:2px;text-align: left;'>Description</th>
-                                <th style='width: 10%;padding:2px;text-align: left;'>Qty</th>
-                                <th style='width: 15%;padding:2px;text-align: left;'>Price</th>
-                                <th style='width: 15%;padding:2px;text-align: left;'>Total</th>
-                                <th style='width: 15%;padding:2px;text-align: left;'>Tax Total</th>
-                                <th style='width: 5%;padding:2px;text-align: left;'>Tax %</th>
+                                <th style='width: 350px;padding:2px;text-align: left;'>Description</th>
+                                <th style='width: 100px;padding:2px;text-align: left;'>Qty</th>
+                                <th style='width: 150px;padding:2px;text-align: left;'>Price</th>
+                                <th style='width: 150px;padding:2px;text-align: left;'>Total</th>
+                                <th style='width: 150px;padding:2px;text-align: left;'>Tax Total</th>
+                                <th style='width: 50px;padding:2px;text-align: left;'>Tax %</th>
                             </tr>";
                             foreach($invoiceLines as $invoiceLine){  
                                 $productDesc = $invoiceLine->Product->ProductDescription;
@@ -110,42 +110,42 @@ class InvoiceTemplate{
 
                                 $htmlTemplate .= "
                                 <tr style='text-align: left;border-bottom: 2px solid rgb(122, 120, 120);'>
-                                    <td style='width: 35%;padding:2px;text-align: left;'>$productDesc</td>
-                                    <td style='width: 10%;padding:2px;text-align: left;'>$invoiceLine->OrderQuantity</td>
-                                    <td style='width: 10%;padding:2px;text-align: left;'>$unitPrice</td>
-                                    <td style='width: 15%;padding:2px;text-align: left;'>$lineTotal</td>
-                                    <td style='width: 15%;padding:2px;text-align: left;'>$lineTax</td>
-                                    <td style='width: 10%;padding:2px;text-align: left;'>$taxRate%</td>
+                                    <td style='width: 350px;padding:2px;text-align: left;'>$productDesc</td>
+                                    <td style='width: 100px;padding:2px;text-align: left;'>$invoiceLine->OrderQuantity</td>
+                                    <td style='width: 100px;padding:2px;text-align: left;'>$unitPrice</td>
+                                    <td style='width: 150px;padding:2px;text-align: left;'>$lineTotal</td>
+                                    <td style='width: 150px;padding:2px;text-align: left;'>$lineTax</td>
+                                    <td style='width: 100px;padding:2px;text-align: left;'>$taxRate%</td>
                                 </tr>";                                
                             }
                         $htmlTemplate .= "
                         </table>
-                        <table style='width: 40%;margin-top: 10px; margin-left: 55%;'>
+                        <table style='width: 400px;margin-top: 10px; margin-left: 550px;border-collapse: collapse;'>
                             <tr style='text-align: left;'>
-                                <td style='width: 20%;padding:2px;'><b>SUBTOTAL (KES)</b></td>
-                                <td style='width: 20%;padding:2px;'>$subTotal</td>
+                                <td style='width: 200px;padding:2px;'><b>SUBTOTAL (KES)</b></td>
+                                <td style='width: 200px;padding:2px;'>$subTotal</td>
                             </tr>
                             <tr style='text-align: left;'>
-                                <td style='width: 20%;padding:2px;'><b>CHARGE SUBTOTAL (KES)</b></td>
-                                <td style='width: 20%;padding:2px;'>0.00</td>
+                                <td style='width: 200px;padding:2px;'><b>CHARGE SUBTOTAL (KES)</b></td>
+                                <td style='width: 200px;padding:2px;'>0.00</td>
                             </tr>
                             <tr style='text-align: left;'>
-                                <td style='width: 20%;padding:2px;'><b>TAX (KES)</b></td>
-                                <td style='width: 20%;padding:2px;'>$taxTotal</td>
+                                <td style='width: 200px;padding:2px;'><b>TAX (KES)</b></td>
+                                <td style='width: 200px;padding:2px;'>$taxTotal</td>
                             </tr>
                             <tr style='text-align: left;border-top: 2px solid rgb(122, 120, 120);border-top: 2px solid rgb(122, 120, 120);'>
-                                <td style='width: 20%;padding:2px;'><b>TOTAL INCL. TAX (KES)</b></td>
-                                <td style='width: 20%;padding:2px;font-weight: bold;'>$total</td>
+                                <td style='width: 200px;padding:2px;'><b>TOTAL INCL. TAX (KES)</b></td>
+                                <td style='width: 200px;padding:2px;font-weight: bold;'>$total</td>
                             </tr>
                         </table>
-                        <table style='width: 100%;margin-top: 10px;'>
+                        <table style='width: 950px;margin-top: 10px;'>
                             <tr style='text-align: left;'>
-                                <td style='width:50%;'>
+                                <td style='width:500px;'>
                                     <div>
                                         <div style='font-weight: bold;'>Due Date <span style='margin-left: 20px;'>$invoiceDueDate</span></div>
                                         <div style='margin-top: 10px;font-weight: bold;'>Payment Terms: <span style='margin-left: 20px; font-weight: normal;'>$paymentTerm</span></div>
                                         <div style='margin-top: 10px;font-weight: bold;'>Payment Details:
-                                            <ul style='list-style: none; margin: 5px;'>                    
+                                            <ul style='margin-left: 40px; list-style: none; margin: 5px;'>                    
                                                 <li>Bank: Diamond Trust Bank</li>
                                                 <li>Branch: Westgate (006) · 0433678002 (KES) or 0433678001 (USD)</li>
                                                 <li>Cheque: Spring Valley Coffee Roasters Limited</li>
@@ -160,7 +160,7 @@ class InvoiceTemplate{
                                         </div>
                                     </div>
                                 </td>            
-                                <td style='width: 35%;margin-left: 10%;'>
+                                <td style='width: 350px;margin-left: 50px;'>
                                     <h5 style='margin:0px;'>KRA QR CODE</h5>
                                     <img width='250px' height='200px' src='".$qrCodePath."'/>
                                 </td>
